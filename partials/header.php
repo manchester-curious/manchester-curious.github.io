9c5ab3c2087f305c8141b562c795743fd49b18eb
George <webbyhall@gmail.com>
2016-03-11 22:02:30 +0000

    Initial Commit

---
+++ partials/header.php
@@ -0,0 +1,22 @@
+
+<header>
+    <div class="logo">
+        <a href="/">Manchester Curious</a>
+    </div>
+    <nav>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Programme</a></li>
+            <li><a href="#">Buildings &amp; Tours</a></li>
+            <li><a href="#">Map</a></li>
+            <li><a href="#">Event Planner</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Want To Get Involved</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+
---
+++ partials/js.php
@@ -0,0 +1,4 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/foundation.core.js"></script>
+<script src="js/foundation.offcanvas.js"></script> 
+<script src="js/app.js"></script>
