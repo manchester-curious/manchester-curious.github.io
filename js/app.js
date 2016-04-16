b82e108bb40e13f217c733b72c8e0b81d43f8fc7
James <jameselliot@gmail.com>
2016-04-16 23:29:26 +0100

    Fetch JSON and store in local storage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
 $(document).foundation();
+
+var BUILDINGS_CACHE_KEY = 'buildings';
+var EVENTS_CACHE_KEY = 'events';
+var pageCache = {};
+
+function getObject(cacheKey, url) {
+	
+	if (localStorage) {
+		var dfd = $.Deferred();
+		var object = localStorage.getCacheItem(cacheKey);
+		if (object) {
+			return $.when(object);
+		}
+	}
+
+	return $.when($.ajax({
+		 url: url,
+		 crossDomain: true
+	})).then(function(data) {
+		if (localStorage) {
+			localStorage.setCacheItem(cacheKey, data, {hours: 1});
+		}
+		return data;
+	});
+}
+
+function getAllTours() {
+	if (!pageCache.tours) {
+		pageCache.tours = getObject(BUILDINGS_CACHE_KEY, "http://buildings.manchestercurious.com/wp-json/wp/v2/buildings");
+	}
+
+	return pageCache.tours;
+}
+
+function getAllEvents() {
+	if (!pageCache.events) {
+		pageCache.events = getObject(EVENTS_CACHE_KEY, "http://buildings.manchestercurious.com/wp-json/wp/v2/events");
+	}
+	return pageCache.events;
+}
+
+$(document).ready(function() {
+	getAllTours();
+	getAllEvents();
+});
+
