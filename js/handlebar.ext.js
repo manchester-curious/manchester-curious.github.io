22f66e603453a4dd4f42a6d3b8c21359042674a9
James <jameselliot@gmail.com>
2016-04-17 10:03:15 +0100

    The tour type is under event_type and not type

--- js/handlebar.ext.js
+++ js/handlebar.ext.js
@@ -1,3 +1,7 @@
 Handlebars.registerHelper('capitalise', function(str) {
   return _.capitalize(str);
 });
+
+Handlebars.registerHelper('removeWhitespace', function(str) {
+  return _.replace(str, ' ', '');
+});
