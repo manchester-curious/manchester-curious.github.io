aa550db0122698751040a0be57c09988b86b1552
James <jameselliot@gmail.com>
2016-04-17 10:10:19 +0100

    Handlebars only allows one helper

--- js/handlebar.ext.js
+++ js/handlebar.ext.js
@@ -2,10 +2,6 @@ Handlebars.registerHelper('capitalise', function(str) {
   return _.capitalize(str);
 });
 
-Handlebars.registerHelper('removeWhitespace', function(str) {
-  return _.replace(str, ' ', '');
-});
-
-Handlebars.registerHelper('toLowercase', function(str) {
-  return _.lowerCase(str);
+Handlebars.registerHelper('toCssClass', function(str) {
+  return _.replace(_.lowerCase(str), ' ', '');
 });
