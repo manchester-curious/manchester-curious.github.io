b644dcefd97ff5421d31aab0ed86fb09b1082dd0
James <jameselliot@gmail.com>
2016-04-27 11:49:41 +0100

    JR - TODO list from tech meeting
    Adding missing text where required to be reviewed by Em.
    Changing duration to be minutes
    Combining accessibility and additional info

--- js/handlebar.ext.js
+++ js/handlebar.ext.js
@@ -5,3 +5,31 @@ Handlebars.registerHelper('capitalise', function(str) {
 Handlebars.registerHelper('toCssClass', function(str) {
   return _.replace(_.lowerCase(str), ' ', '');
 });
+
+
+Handlebars.registerHelper('toDuration', function(str) {
+	function toHours(hours) {
+		if (hours == 1) {
+			return '1 hour';
+		} else {
+			return hours + ' hours';
+		}
+	}
+
+	var totalMinutes = parseInt(str);
+	if (isNaN(totalMinutes)) {
+		return '';
+	}
+	var hours = Math.floor( totalMinutes / 60);          
+    var minutes = totalMinutes % 60;
+
+    if (hours > 0) {
+    	if (minutes > 0) {
+    		return toHours(hours) + ' ' + minutes + ' minutes';
+    	} else {
+    		return toHours(hours);
+    	}
+    }
+
+    return minutes + ' minutes';
+});
