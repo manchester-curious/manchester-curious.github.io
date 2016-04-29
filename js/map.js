e4fe49548782c0125acbf34b203e1062bac79e97
Mark Crossfield <github@nanunanu.co.uk>
2016-04-30 00:19:11 +0100

    Adds a simple map with each building plotted.

---
+++ js/map.js
@@ -0,0 +1,24 @@
+function initialiseMap(mapContainerId) {
+    L.mapbox.accessToken = 'pk.eyJ1IjoibWFuY2hlc3RlcmN1cmlvdXNtYXAiLCJhIjoiY2lubTZsY2V1MDBiNXcya2wwc3FyNjJsaiJ9.Ra0-KaX_IvxzcTfRMoyBGQ';
+    var map = L.mapbox.map(mapContainerId, 'mapbox.light', {}).setView([53.479144, -2.244161], 12);
+    var rootUrl = window.location.protocol + '//' + window.location.host + '/';
+    getAllTours().then(function (tours) {
+        var geoJSON = {'type': 'FeatureCollection', 'features': []}
+        for (var tourIndex = 0; tourIndex < tours.length; tourIndex++) {
+            var tour = tours[tourIndex];
+            geoJSON.features.push(
+                {
+                    'type': 'Feature',
+                    'properties': {
+                        'title': tour.title.rendered,
+                        'description': tour.content.rendered + '<br><a href="' + rootUrl + 'tour-details.html?tour=' + tour.slug + '">Read more</a>'
+                    },
+                    "geometry": {
+                        "coordinates": [Number(tour.longitude), Number(tour.latitude)],
+                        "type": "Point"
+                    }
+                });
+        }
+        L.mapbox.featureLayer(geoJSON).addTo(map);
+    });
+}
